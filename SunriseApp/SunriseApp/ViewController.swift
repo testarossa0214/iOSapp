6af0d5748dca88e60459b1731cee50d5fe13014f
testarossa0214 <soundlight@live.jp>
2021-09-04 19:07:47 +0900

    日の出アプリ

---
+++ SunriseApp/SunriseApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SunriseApp
+//
+//  Created by 柳生宗矩 on 2021/09/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cityNameInput: UITextField!
+    
+    @IBOutlet weak var sunriseTimeLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func findSunrise(_ sender: Any) {
+    }
+}
+
